9a71f952b8a29e6247af72d62f1ae05fd9dee0fa
Arinze Chinweuba <calistusarinzechinweuba@gmail.com>
2025-10-02 09:46:31 +0100

    preloader commit

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from "./+types/root";
 import "./globals.css";
 import { Navigation } from "./components/layout/navigation";
 import { Footer } from "./components/layout/footer";
+import { Preloader } from "./components/preloader";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -35,6 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Preloader />
         <Navigation />
         {children}
         <Footer />
